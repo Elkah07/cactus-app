1f645a2dcf4465bb323c13c243497ef2e7dc74c8
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-06-16 17:10:08 +0200

    Mise à jour cache PWA

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "cactus-v1";
+const CACHE_NAME = "cactus-v3";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./css/style.css",
+    "./js/firebase.js",
     "./js/script.js",
     "./js/screens.js",
     "./js/utils.js",
@@ -15,6 +16,8 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", (event) => {
+    self.skipWaiting();
+
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(FILES_TO_CACHE);
@@ -22,10 +25,26 @@ self.addEventListener("install", (event) => {
     );
 });
 
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
         })
     );
 });
