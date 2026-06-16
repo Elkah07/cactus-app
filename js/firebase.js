e674d42933cf5fd2b9d3122e2fc16510167d37e4
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-06-16 16:47:20 +0200

    Connexion Firebase

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsieGW6fu5RmTrmrrumIoSOm600HSn160",
+  authDomain: "couple-game-9f881.firebaseapp.com",
+  databaseURL: "https://couple-game-9f881-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "couple-game-9f881",
+  storageBucket: "couple-game-9f881.firebasestorage.app",
+  messagingSenderId: "2640989123",
+  appId: "1:2640989123:web:05b0d586bfeec1bb598b76"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+console.log("🔥 Firebase connecté !");
