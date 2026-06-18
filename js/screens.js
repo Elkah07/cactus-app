cfa05610c3f8ef02400e3717356329b7bcef7e07
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-06-18 12:46:30 +0200

    guess my answer

--- js/screens.js
+++ js/screens.js
@@ -1,19 +1,37 @@
 function showScreen(screenName) {
+
+    // ====================
+    // MASQUER TOUS LES ÉCRANS
+    // ====================
+
     loginScreen.style.display = "none";
     pseudoScreen.style.display = "none";
     coupleScreen.style.display = "none";
     dashboardScreen.style.display = "none";
+
     rankingScreen.style.display = "none";
     rankingResultScreen.style.display = "none";
+    rankingCompatibilityScreen.style.display = "none";
+
     settingsScreen.style.display = "none";
     historyScreen.style.display = "none";
+
     gardenScreen.style.display = "none";
-    rankingCompatibilityScreen.style.display = "none";
+    notebookScreen.style.display = "none";
+
+    guessAnswerScreen.style.display = "none";
+    guessWaitingScreen.style.display = "none";
+    guessPredictScreen.style.display = "none";
+    guessValidationScreen.style.display = "none";
+    guessResultScreen.style.display = "none";
+
+    // ====================
+    // BOUTONS DU HAUT
+    // ====================
 
     historyBtn.style.display = "none";
     gardenBtn.style.display = "none";
     settingsBtn.style.display = "none";
-    notebookScreen.style.display = "none";
 
     if (
         screenName === "dashboard" ||
@@ -26,47 +44,78 @@ function showScreen(screenName) {
         settingsBtn.style.display = "flex";
     }
 
-    if (screenName === "login") {
-        loginScreen.style.display = "block";
-    }
+    // ====================
+    // AFFICHAGE ÉCRAN DEMANDÉ
+    // ====================
 
-    if (screenName === "pseudo") {
-        pseudoScreen.style.display = "block";
-    }
+    switch (screenName) {
 
-    if (screenName === "couple") {
-        coupleScreen.style.display = "block";
-    }
+        case "login":
+            loginScreen.style.display = "block";
+            break;
 
-    if (screenName === "dashboard") {
-        dashboardScreen.style.display = "block";
-    }
+        case "pseudo":
+            pseudoScreen.style.display = "block";
+            break;
 
-    if (screenName === "ranking") {
-        rankingScreen.style.display = "block";
-    }
+        case "couple":
+            coupleScreen.style.display = "block";
+            break;
 
-    if (screenName === "rankingResult") {
-        rankingResultScreen.style.display = "block";
-    }
+        case "dashboard":
+            dashboardScreen.style.display = "block";
+            break;
 
-    if (screenName === "settings") {
-        settingsScreen.style.display = "block";
-    }
+        case "ranking":
+            rankingScreen.style.display = "block";
+            break;
 
-    if (screenName === "history") {
-        historyScreen.style.display = "block";
-    }
+        case "rankingResult":
+            rankingResultScreen.style.display = "block";
+            break;
 
-    if (screenName === "garden") {
-        gardenScreen.style.display = "block";
-    }
+        case "rankingCompatibility":
+            rankingCompatibilityScreen.style.display = "block";
+            break;
 
-    if (screenName === "rankingCompatibility") {
-        rankingCompatibilityScreen.style.display = "block";
-    }
+        case "settings":
+            settingsScreen.style.display = "block";
+            break;
 
-    if (screenName === "notebook") {
-    notebookScreen.style.display = "block";
-}
+        case "history":
+            historyScreen.style.display = "block";
+            break;
+
+        case "garden":
+            gardenScreen.style.display = "block";
+            break;
+
+        case "notebook":
+            notebookScreen.style.display = "block";
+            break;
+
+        // ====================
+        // DEVINE MA RÉPONSE
+        // ====================
+
+        case "guessAnswer":
+            guessAnswerScreen.style.display = "block";
+            break;
+
+        case "guessWaiting":
+            guessWaitingScreen.style.display = "block";
+            break;
+
+        case "guessPredict":
+            guessPredictScreen.style.display = "block";
+            break;
+
+        case "guessValidation":
+            guessValidationScreen.style.display = "block";
+            break;
+
+        case "guessResult":
+            guessResultScreen.style.display = "block";
+            break;
+    }
 }
