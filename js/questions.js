3cbc71fa78453bec3b639f24cdd75b3551d3fc3c
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-06-19 00:21:38 +0200

    pl

--- js/questions.js
+++ js/questions.js
@@ -1,8 +1,1 @@
 // Ce fichier servira plus tard pour le mode Questions.
-let likelyQuestions = [];
-
-fetch("data/likely.json")
-    .then((response) => response.json())
-    .then((data) => {
-        likelyQuestions = data;
-    });
