27cf64b8154adee96f32db5b73aaf7510a1b177e
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-06-28 20:53:31 +0200

    maj

--- js/screens.js
+++ js/screens.js
@@ -14,247 +14,235 @@ function showScreen(screenName) {
     showScreenContent(screenName);
 }
 
+function hideScreen(screen) {
+    if (screen) {
+        screen.style.display = "none";
+    }
+}
+
 function showScreenContent(screenName) {
+    hideScreen(loginScreen);
+    hideScreen(pseudoScreen);
+    hideScreen(coupleScreen);
+    hideScreen(dashboardScreen);
 
-    // ====================
-    // MASQUER TOUS LES ÉCRANS
-    // ====================
+    hideScreen(rankingScreen);
+    hideScreen(rankingResultScreen);
+    hideScreen(rankingCompatibilityScreen);
 
-    loginScreen.style.display = "none";
-    pseudoScreen.style.display = "none";
-    coupleScreen.style.display = "none";
-    dashboardScreen.style.display = "none";
+    hideScreen(settingsScreen);
+    hideScreen(historyScreen);
+    hideScreen(historyDetailScreen);
+    hideScreen(historyItemScreen);
 
-    rankingScreen.style.display = "none";
-    rankingResultScreen.style.display = "none";
-    rankingCompatibilityScreen.style.display = "none";
+    hideScreen(gardenScreen);
+    hideScreen(notebookScreen);
 
-    settingsScreen.style.display = "none";
-    historyScreen.style.display = "none";
+    hideScreen(guessAnswerScreen);
+    hideScreen(guessWaitingScreen);
+    hideScreen(guessPredictScreen);
+    hideScreen(guessValidationScreen);
+    hideScreen(guessResultScreen);
 
-    gardenScreen.style.display = "none";
-    notebookScreen.style.display = "none";
+    hideScreen(likelyScreen);
+    hideScreen(likelyResultScreen);
 
-    guessAnswerScreen.style.display = "none";
-    guessWaitingScreen.style.display = "none";
-    guessPredictScreen.style.display = "none";
-    guessValidationScreen.style.display = "none";
-    guessResultScreen.style.display = "none";
+    hideScreen(okScreen);
+    hideScreen(okResultScreen);
 
-    likelyScreen.style.display = "none";
-    likelyResultScreen.style.display = "none";
+    hideScreen(greenFlagScreen);
+    hideScreen(greenFlagResultScreen);
 
-    okScreen.style.display = "none";
-    okResultScreen.style.display = "none";
+    hideScreen(princessScreen);
+    hideScreen(princessResultScreen);
 
-    greenFlagScreen.style.display = "none";
-    greenFlagResultScreen.style.display = "none";
+    hideScreen(questionsScreen);
+    hideScreen(questionsResultScreen);
 
-    princessScreen.style.display = "none";
-    princessResultScreen.style.display = "none";
+    hideScreen(answerSentScreen);
 
-    questionsScreen.style.display = "none";
-    questionsResultScreen.style.display = "none";
-
-    answerSentScreen.style.display = "none";
-
-    historyDetailScreen.style.display = "none";
-
-    historyItemScreen.style.display = "none";
-
-storyIntroScreen.style.display = "none";
-storyDateScreen.style.display = "none";
-storyMeetingScreen.style.display = "none";
-storyFirstDateScreen.style.display = "none";
-storyNicknamesScreen.style.display = "none";
-storySongScreen.style.display = "none";
-storyDistanceScreen.style.display = "none";
-storyFinalScreen.style.display = "none";
-
-    // ====================
-    // BOUTONS DU HAUT
-    // ====================
+    hideScreen(storyIntroScreen);
+    hideScreen(storyDateScreen);
+    hideScreen(storyMeetingScreen);
+    hideScreen(storyFirstDateScreen);
+    hideScreen(storyNicknamesScreen);
+    hideScreen(storySongScreen);
+    hideScreen(storyDistanceScreen);
+    hideScreen(storyFinalScreen);
 
     historyBtn.style.display = "none";
     gardenBtn.style.display = "none";
     settingsBtn.style.display = "none";
 
     if (
-    screenName === "dashboard" ||
-    screenName === "history" ||
-    screenName === "historyDetail" ||
-    screenName === "historyItem" ||
-    screenName === "garden" ||
-    screenName === "settings"
-)
-
-{
+        screenName === "dashboard" ||
+        screenName === "history" ||
+        screenName === "historyDetail" ||
+        screenName === "historyItem" ||
+        screenName === "garden" ||
+        screenName === "settings"
+    ) {
         historyBtn.style.display = "flex";
         gardenBtn.style.display = "flex";
         settingsBtn.style.display = "flex";
     }
 
-    // ====================
-    // AFFICHAGE ÉCRAN DEMANDÉ
-    // ====================
+    switch (screenName) {
+        case "login":
+            loginScreen.style.display = "block";
+            break;
 
-switch (screenName) {
-    case "login":
-        loginScreen.style.display = "block";
-        break;
+        case "pseudo":
+            pseudoScreen.style.display = "block";
+            break;
 
-    case "pseudo":
-        pseudoScreen.style.display = "block";
-        break;
+        case "couple":
+            coupleScreen.style.display = "block";
+            break;
 
-    case "couple":
-        coupleScreen.style.display = "block";
-        break;
+        case "dashboard":
+            dashboardScreen.style.display = "block";
+            loadCoupleStats();
+            break;
 
-    case "dashboard":
-        dashboardScreen.style.display = "block";
-        loadCoupleStats();
-        break;
+        case "ranking":
+            rankingScreen.style.display = "block";
+            break;
 
-    case "ranking":
-        rankingScreen.style.display = "block";
-        break;
+        case "rankingResult":
+            rankingResultScreen.style.display = "block";
+            break;
 
-    case "rankingResult":
-        rankingResultScreen.style.display = "block";
-        break;
+        case "rankingCompatibility":
+            rankingCompatibilityScreen.style.display = "block";
+            break;
 
-    case "rankingCompatibility":
-        rankingCompatibilityScreen.style.display = "block";
-        break;
+        case "settings":
+            settingsScreen.style.display = "block";
+            break;
 
-    case "settings":
-        settingsScreen.style.display = "block";
-        break;
+        case "history":
+            historyScreen.style.display = "block";
+            break;
 
-    case "history":
-        historyScreen.style.display = "block";
-        break;
+        case "historyDetail":
+            historyDetailScreen.style.display = "block";
+            break;
 
-    case "historyDetail":
-        historyDetailScreen.style.display = "block";
-        break;
+        case "historyItem":
+            historyItemScreen.style.display = "block";
+            break;
 
-    case "historyItem":
-        historyItemScreen.style.display = "block";
-        break;
+        case "garden":
+            gardenScreen.style.display = "block";
+            break;
 
-    case "garden":
-        gardenScreen.style.display = "block";
-        break;
+        case "notebook":
+            notebookScreen.style.display = "block";
+            break;
 
-    case "notebook":
-        notebookScreen.style.display = "block";
-        break;
+        case "guessAnswer":
+            guessAnswerScreen.style.display = "block";
+            break;
 
-    case "guessAnswer":
-        guessAnswerScreen.style.display = "block";
-        break;
+        case "guessWaiting":
+            guessWaitingScreen.style.display = "block";
+            break;
 
-    case "guessWaiting":
-        guessWaitingScreen.style.display = "block";
-        break;
+        case "guessPredict":
+            guessPredictScreen.style.display = "block";
+            break;
 
-    case "guessPredict":
-        guessPredictScreen.style.display = "block";
-        break;
+        case "guessValidation":
+            guessValidationScreen.style.display = "block";
+            break;
 
-    case "guessValidation":
-        guessValidationScreen.style.display = "block";
-        break;
+        case "guessResult":
+            guessResultScreen.style.display = "block";
+            break;
 
-    case "guessResult":
-        guessResultScreen.style.display = "block";
-        break;
+        case "likely":
+            likelyScreen.style.display = "block";
+            break;
 
-    case "likely":
-        likelyScreen.style.display = "block";
-        break;
+        case "likelyResult":
+            likelyResultScreen.style.display = "block";
+            break;
 
-    case "likelyResult":
-        likelyResultScreen.style.display = "block";
-        break;
+        case "ok":
+            okScreen.style.display = "block";
+            break;
 
-    case "ok":
-        okScreen.style.display = "block";
-        break;
+        case "okResult":
+            okResultScreen.style.display = "block";
+            break;
 
-    case "okResult":
-        okResultScreen.style.display = "block";
-        break;
+        case "greenFlag":
+            greenFlagScreen.style.display = "block";
+            break;
 
-    case "greenFlag":
-        greenFlagScreen.style.display = "block";
-        break;
+        case "greenFlagResult":
+            greenFlagResultScreen.style.display = "block";
+            break;
 
-    case "greenFlagResult":
-        greenFlagResultScreen.style.display = "block";
-        break;
+        case "princess":
+            princessScreen.style.display = "block";
+            break;
 
-    case "princess":
-        princessScreen.style.display = "block";
-        break;
+        case "princessResult":
+            princessResultScreen.style.display = "block";
+            break;
 
-    case "princessResult":
-        princessResultScreen.style.display = "block";
-        break;
+        case "questions":
+            questionsScreen.style.display = "block";
+            break;
 
-    case "questions":
-        questionsScreen.style.display = "block";
-        break;
+        case "questionsResult":
+            questionsResultScreen.style.display = "block";
+            break;
 
-    case "questionsResult":
-        questionsResultScreen.style.display = "block";
-        break;
+        case "answerSent":
+            answerSentScreen.style.display = "block";
+            break;
 
-    case "answerSent":
-        answerSentScreen.style.display = "block";
-        break;
+        case "storyIntro":
+            storyIntroScreen.style.display = "block";
+            break;
 
-    case "storyIntro":
-        storyIntroScreen.style.display = "block";
-        break;
+        case "storyDate":
+            storyDateScreen.style.display = "block";
+            break;
 
-    case "storyDate":
-        storyDateScreen.style.display = "block";
-        break;
+        case "storyMeeting":
+            storyMeetingScreen.style.display = "block";
+            break;
 
-    case "storyMeeting":
-        storyMeetingScreen.style.display = "block";
-        break;
+        case "storyFirstDate":
+            storyFirstDateScreen.style.display = "block";
+            break;
 
-    case "storyFirstDate":
-        storyFirstDateScreen.style.display = "block";
-        break;
+        case "storyNicknames":
+            storyNicknamesScreen.style.display = "block";
+            break;
 
-    case "storyNicknames":
-        storyNicknamesScreen.style.display = "block";
-        break;
+        case "storySong":
+            storySongScreen.style.display = "block";
+            break;
 
-    case "storySong":
-        storySongScreen.style.display = "block";
-        break;
+        case "storyDistance":
+            storyDistanceScreen.style.display = "block";
+            break;
 
-    case "storyDistance":
-        storyDistanceScreen.style.display = "block";
-        break;
+        case "storyFinal":
+            storyFinalScreen.style.display = "block";
+            break;
 
-    case "storyFinal":
-        storyFinalScreen.style.display = "block";
-        break;
-
-    default:
-        dashboardScreen.style.display = "block";
-        lastShownScreen = "dashboard";
-        break;
+        default:
+            dashboardScreen.style.display = "block";
+            lastShownScreen = "dashboard";
+            break;
+    }
 }
-
-
 
 window.addEventListener("popstate", (event) => {
     if (!event.state || !event.state.screen) {
