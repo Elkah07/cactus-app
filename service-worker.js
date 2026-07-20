9a4cb57c0139d4a7589153299eb86f08b251404c
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-07-20 20:12:19 +0200

    maj bc

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_VERSION = "v66";
+const CACHE_VERSION = "v67";
 const SHELL_CACHE = `cactus-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `cactus-runtime-${CACHE_VERSION}`;
 
 const APP_SHELL = [
     "./index.html",
-    "./css/style.css?v=65",
+    "./css/style.css?v=67",
     "./js/firebase.js",
     "./js/script.js?v=66",
     "./js/screens.js?v=61",
