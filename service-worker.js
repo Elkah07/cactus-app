0018535e26d6226922fdce37702e7ceaf737324b
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-07-22 11:17:39 +0200

    maj visu dashboard

--- service-worker.js
+++ service-worker.js
@@ -88,13 +88,13 @@ try {
     console.warn("Firebase Messaging indisponible dans le service worker", error);
 }
 
-const CACHE_VERSION = "v106";
+const CACHE_VERSION = "v107";
 const SHELL_CACHE = `cactus-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `cactus-runtime-${CACHE_VERSION}`;
 
 const APP_SHELL = [
     "./index.html",
-    "./css/style.css?v=106",
+    "./css/style.css?v=107",
     "./js/firebase.js?v=86",
     "./js/script.js?v=105",
     "./js/screens.js?v=84",
