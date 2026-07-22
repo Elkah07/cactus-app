d189bbb6c0b33fd785064ec78515a3652c9d4fc8
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-07-22 16:02:43 +0200

    maj

--- service-worker.js
+++ service-worker.js
@@ -88,7 +88,7 @@ try {
     console.warn("Firebase Messaging indisponible dans le service worker", error);
 }
 
-const CACHE_VERSION = "v115";
+const CACHE_VERSION = "v1151";
 const SHELL_CACHE = `cactus-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `cactus-runtime-${CACHE_VERSION}`;
 
